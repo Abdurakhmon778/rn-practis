68572c8974114c495940494140001c8e4856545e
Abdumutal Abdusamatov <abdusamatov@mercurydevelopment.com>
2022-08-18 20:15:46 +0500

    added DatePicker

--- App.tsx
+++ App.tsx
@@ -1,55 +1,32 @@
-import React from "react"
-import {
-  View,
-  Text,
-  StyleSheet,
-  StatusBar,
-  TouchableOpacity
-} from 'react-native'
+import React, { useState } from 'react'
+import { Button, Text, View } from 'react-native'
+import DatePicker from 'react-native-date-picker'
 
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import BottomSheet, { BottomSheetRefProps } from "./components/BottomSheet"
-
-interface RenderAppProps {
-  children: React.ReactNode
-}
-
-const App = () => {
-  const ref = React.useRef<BottomSheetRefProps>(null)
-  const onPress = React.useCallback(() => {
-    const isActive = ref?.current?.isActive();
-    ref?.current?.scrollTo(isActive ? 0 : -100)
-  }, [])
-
-  const RenderApp: React.FC<RenderAppProps> = ({ children }) => (
-    <GestureHandlerRootView style={{ flex: 1 }}>{children}</GestureHandlerRootView>
-  )
+export default function App() {
+  const [date, setDate] = useState(new Date())
+  const [open, setOpen] = useState(false)
 
   return (
-    <RenderApp>
-      <View style={styles.container}>
-        <StatusBar barStyle={"light-content"} />
-        <TouchableOpacity style={styles.button} onPress={onPress} />
-        <BottomSheet ref={ref} />
-      </View>
-    </RenderApp>
+    <View style={{
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "#fff"
+    }}>
+      <Text style={{ color: "red" }}>{date + ""}</Text>
+      <Button title="Open" onPress={() => setOpen(true)} />
+      <DatePicker
+        modal
+        open={open}
+        date={date}
+        onConfirm={(date) => {
+          setOpen(false)
+          setDate(date)
+        }}
+        onCancel={() => {
+          setOpen(false)
+        }}
+      />
+    </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "#111"
-  },
-  button: {
-    height: 50,
-    aspectRatio: 1,
-    backgroundColor: "#fff",
-    borderRadius: 25,
-    opacity: 0.6
-  }
-})
-
-export default App
