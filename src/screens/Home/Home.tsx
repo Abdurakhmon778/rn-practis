76f76e0bedea54e6fd7e1adb214ac3fe08f3e1f9
Abdumutal Abdusamatov <abdusamatov@mercurydevelopment.com>
2022-08-26 08:05:57 +0500

    add fileas

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import tw from 'tailwind-react-native-classnames'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import { useNavigation } from "@react-navigation/native"
+import { SafeAreaView } from 'react-native-safe-area-context'
+import tw from 'tailwind-react-native-classnames';
 
 const Home = () => {
     const navigation = useNavigation()
@@ -11,9 +12,24 @@ const Home = () => {
         })
     }, [])
     return (
-        <View>
-            <Text style={tw`text-green-400 `}>Hello</Text>
-        </View>
+        <SafeAreaView>
+            {/* Header */}
+
+            <View style={tw`flex-row pb-3 items-center mx-4`}>
+                <Image
+                    source={{
+                        uri: "https://links.papareact.com/wru"
+                    }}
+                    resizeMode={"contain"}
+                    style={tw`h-7 w-7 bg-gray-300 p-4 rounded-full`}
+                />
+            </View>
+
+            <View style={tw`mr-4`}>
+                <Text style={tw`font-bold text-gray-400 text-xs`}>Deliver Now!</Text>
+                <Text>Current Location</Text>
+            </View>
+        </SafeAreaView>
     )
 }
 
